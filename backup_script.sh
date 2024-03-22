c8f5cd4659df78ec4c428cde31254f3cbf335c1d
bidetaggle <bidetaggle@gmail.com>
2024-03-22 11:11:38 +0000

    clean up

--- backup_script.sh
+++ backup_script.sh
@@ -1,3 +1,5 @@
+HOST="db:6362"
 BACKUP_DIR="/var/lib/neo4j/backups"
+CRON_LOG="/var/lib/neo4j/cron.log"
 
-/var/lib/neo4j/bin/neo4j-admin database backup --to-path=$BACKUP_DIR --from=db:6362 >> /var/lib/neo4j/cron.log 2>&1
+/var/lib/neo4j/bin/neo4j-admin database backup --to-path=$BACKUP_DIR --from=$HOST >> $CRON_LOG 2>&1
