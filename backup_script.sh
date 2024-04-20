5cdc42cacd6e1161260b2050d65e6d8e9769fc28
bidetaggle <bidetaggle@gmail.com>
2024-04-20 13:01:09 +0100

    add backup files rotation

--- backup_script.sh
+++ backup_script.sh
@@ -1,5 +1,10 @@
+MAX_BACKUPS=5
 HOST="db:6362"
 BACKUP_DIR="/var/lib/neo4j/backups"
 CRON_LOG="/var/lib/neo4j/cron.log"
 
 /var/lib/neo4j/bin/neo4j-admin database backup --to-path=$BACKUP_DIR --from=$HOST >> $CRON_LOG 2>&1
+
+# Backup rotation: Keep only the $MAX_BACKUPS most recent backups
+cd $BACKUP_DIR
+ls -tp | grep -v '/$' | tail -n +$((MAX_BACKUPS + 1)) | xargs -d '\n' -r rm --
